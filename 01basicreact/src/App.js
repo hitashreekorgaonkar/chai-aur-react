c6a8e8678fc6f360db4fd2a003ac738294c778e0
hitashreekorgaonkar <hkorgaonkar10@gmail.com>
2023-08-27 00:00:52 +0530

    file structure, custom react and hooks

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,15 @@
+import Chai from "./chai";
+
+function App() {
+  return (
+    <>
+      <Chai />
+      <h1>
+        Chai aur react | app created with <b>npx creat-react-app</b> It is a
+        bulky way of creating an app
+      </h1>
+    </>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,14 @@
+import Chai from "./Chai";
+function App() {
+  return (
+    <>
+      <h1>
+        Chai aur react with vite | app created with vite
+        <p>npm create vite@latest </p> It is not a bulky way of creating an app
+      </h1>
+      <Chai />
+    </>
+  );
+}
+
+export default App;
